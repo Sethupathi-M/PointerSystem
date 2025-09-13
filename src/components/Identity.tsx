e03050edfba3753debf4db70e5fb4d3f78674bd0
Sethupathi <sethupathi.mahendran@gmail.com>
2025-09-13 18:18:48 +0530

    Refactor Identity component to use IdentityProvider instead of IdentityContext, and optimize imports.

--- src/components/Identity.tsx
+++ src/components/Identity.tsx
@@ -3,15 +3,11 @@ import { AddTask } from "@/components/AddTask";
 import { Header } from "@/components/Header/Header";
 import Spinner from "@/components/Spinner";
 import { TaskList } from "@/components/TaskList";
-import type {
-  Identity,
-  Identity as IdentityType,
-  Task,
-} from "@/generated/prisma";
+import type { Identity, Task } from "@/generated/prisma";
 import { identityApi } from "@/lib/api/identityApi";
 import { taskApi } from "@/lib/api/taskApi";
 import { useQuery } from "@tanstack/react-query";
-import { IdentityContext } from "./IdentityContext";
+import { IdentityProvider } from "./IdentityContext";
 
 const Identity = ({ id }: { id: string }) => {
   const { data: identity, isLoading } = useQuery({
@@ -25,7 +21,7 @@ const Identity = ({ id }: { id: string }) => {
   });
 
   return (
-    <IdentityContext.Provider value={identity as IdentityType}>
+    <IdentityProvider>
       <div className="px-8 mt-1 relative w-full flex gap-0 lg:gap-4 h-11/12">
         <main className="flex-1 lg:p-6">
           {isLoading || taskLoading || !identity ? (
@@ -43,7 +39,7 @@ const Identity = ({ id }: { id: string }) => {
           )}
         </main>
       </div>
-    </IdentityContext.Provider>
+    </IdentityProvider>
   );
 };
 
